40fc331062f6fda9f1a9c00cdee757232df4e7f4
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2023-12-18 07:12:47 -0500

    comments added

--- helper/uuid.js
+++ helper/uuid.js
@@ -1,3 +1,4 @@
+//creating a unique id
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+//importing express
 const router = require('express').Router();
+
+//importing notes.js file 
 const notesRouter = require('./notes');
 
+//any request with a URL that begins with "/notes" will be routed to the notesRouter for further handling.
 router.use('/notes', notesRouter);
+
 
 module.exports = router;
