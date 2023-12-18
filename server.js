9623cc4a527d46fd90c4eb4ba1c38238e4274211
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2023-12-18 10:30:50 -0500

    layout fixes

--- server.js
+++ server.js
@@ -10,18 +10,18 @@ const PORT = process.env.PORT || 3001;
 //adding middleware 
 app.use(express.json());
 app.use(express.static('public'));
-app.use('./api', api);
+app.use('./api', api)
 
 //define a route for serving the notes.html file 
 app.get('/notes', (req,res) => 
-res.sendFile(path.join(__dirname, 'public/notes.html')));
+    res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 
 //define a catch-all route for serving the index.html file 
 app.get('*', (req,res) =>
-res.sendFile(path.join(__dirname, 'public/index.html')));
+    res.sendFile(path.join(__dirname, 'public/index.html')));
 
 
 //start the server and listen on the specified port
 app.listen(PORT, () =>
-console.log(`This application is listening at http://localhost:${PORT}`));
+    console.log(`This application is listening at http://localhost:${PORT}`));
