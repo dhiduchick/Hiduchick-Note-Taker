9967b9b9001563cae8770d1df059519157654c93
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2023-12-18 08:39:06 -0500

    comments added

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,16 @@
+//import required modules
 const api = require ('express').Router();
 const uuid = require('../lib/uuid');
 const fs = require('fs');
 let database = require('../db/db.json');
 
+//handle get request to retrieve all notes
 api.get('/', (req,res) => {
     fs.readFile('./db/db.json', 'utf-8', (data) => res.json(JSON.parse(data)))
 })
 
+
+//handle post request to create a new note
 api.post('/', (req,res) => {
     const { title, text } = req.body;
     if (req.body && title && text) {
@@ -15,7 +19,7 @@ api.post('/', (req,res) => {
             text,
             id: uuid()
         }
-
+//read the contens of the database file / write the update data back to the database file 
         fs.readFile('./db/db.json', 'utf-8',(data) => {
             const oldDataArr = JSON.parse(data);
             oldDataArr.push(newNote);
@@ -26,6 +30,7 @@ api.post('/', (req,res) => {
     }
 })
 
+//handles the delete request to delete a note by id 
 api.delete('/:id', (req,res) => {
     let notesKept = [];
     for (let i=0; i<database.length; i++) {
@@ -33,10 +38,11 @@ api.delete('/:id', (req,res) => {
             notesKept.push(database[i]);
         }
     }
-
+//update the database wit hthe notes to be kept 
     database = notesKept;
     fs.writeFileSync('./db/db,json', JSON.stringify(database,null, '\t'))
     res.json(database);
 })
 
+//exports the configured API router for use in other parts of the application 
 module.exports = api;
