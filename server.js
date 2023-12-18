d678b192a2d9a7bc3883440e59618a213c4e98b2
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2023-12-18 08:41:55 -0500

    comments added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+//import required modules 
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
 
+//create an express application 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,11 +12,16 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('./api', api)
 
+//define a route for serving the notes.html file 
 app.get('/notes', (req,res) => 
 res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+
+//define a catch-all route for serving the index.html file 
 app.get('*', (req,res) =>
 res.sendFile(path.join(__dirname, 'public/index.html')));
 
+
+//start the server and listen on the specified port
 app.listen(PORT, () =>
 console.log(`This application is listening at http://localhost:${PORT}`));
